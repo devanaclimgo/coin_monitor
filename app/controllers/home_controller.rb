238536e19337e79e7a5d538fb5a3558364cdb5a7
Ana <anaclimgo@gmail.com>
2025-05-01 18:29:46 -0300

    api with days commented

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,6 @@ class HomeController < ApplicationController
   def index
     @chart_data = [["2025-01-01" => 2], ["2025-01-02" => 3]]
 
-    # https://economia.awesomeapi.com.br/json/last/:moedas
+    # https://economia.awesomeapi.com.br/json/daily/:moeda/:numero_dias
   end
 end
