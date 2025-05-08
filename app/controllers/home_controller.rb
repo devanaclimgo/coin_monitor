634fc47d889319ef456eced102cbafc314d32739
Ana <anaclimgo@gmail.com>
2025-05-07 22:48:10 -0300

    refactor(api): remove strftime and use raw timestamp in API response

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -15,7 +15,7 @@ class HomeController < ApplicationController
 
     hash = {}
     data.each do |entry|
-      date = Time.at(entry['timestamp'].to_i).strftime("%d/%m/%Y")
+      date = Time.at(entry['timestamp'].to_i)
       rate = entry['high']
 
       hash[date] = rate
